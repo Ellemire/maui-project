6041c4f651703d4849e731fa59cdb9a444bdee8b
Michał <mkrok3@gmail.com>
2026-01-18 19:36:43 +0100

    Added .slnx file and three empty projects: Client, Server and Shared. Adjusted .gitignore file

---
+++ the-oasis/TheOasis.Client/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TheOasis.Client
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ the-oasis/TheOasis.Client/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TheOasis.Client
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
